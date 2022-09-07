c29acb455eba4016c9ec09e097e732e858a8f42a
Harikrushna Sangani <harikrushna.sangani@softices.com>
2022-09-07 14:23:37 +0530

    change variable: class to global

--- Ch3/main/exe_2.rb
+++ Ch3/main/exe_2.rb
@@ -7,13 +7,10 @@ class Dimention
   @@min_l = 200
   @@img_width = Array[]
   @@img_height = Array[]
-  @@num_of_img
 
   def initialize()
-    puts "Enter the number of image you wants to upload : "
-    @@num_of_img = gets.chomp.to_i
     i = 0
-    while i < @@num_of_img do
+    while i < $num_of_img do
       puts "Enter width and height of image #{i + 1}: "
       @user_input = gets.chomp.split(' ')
       @@img_width.push(@user_input[0].to_i)
@@ -24,7 +21,7 @@ class Dimention
 
   def validity()
     i = 0
-    while i < @@num_of_img do
+    while i < $num_of_img do
       if @@img_width[i] == @@img_height[i]
         puts "Perfect"
       elsif @@img_width[i] < @@min_l || @@img_height[i] < @@min_l
@@ -37,5 +34,7 @@ class Dimention
   end
 end
 
+puts "Enter the number of image you wants to upload : "
+$num_of_img = gets.chomp.to_i
 obj = Dimention.new()
 obj.validity()
